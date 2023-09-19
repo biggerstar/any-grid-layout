840c4bc758652a9c5d5c3ae2cac2c30da1c102e8
biggerstar <1458048116@qq.com>
2023-09-19 16:14:13 +0800

    feat: 支持event$ 事件周期结束事件

--- packages/native/src/plugins/PluginManager.ts
+++ packages/native/src/plugins/PluginManager.ts
@@ -30,7 +30,8 @@ export class PluginManager {
     const container = this.container
     const GEvent = EventMap[eventName] || EventMap['*']
     const defaultActionFn: Function = DefaultBehavior[eventName]
-    const $defaultActionFn: Function = DefaultBehavior[`$${eventName}`]   // 内置用，在事件对象实例化所有插件回调前调用,不可被阻止
+    const $defaultActionFn: Function = DefaultBehavior[`$${eventName}`]   // [内置用] 在事件对象实例化所有插件回调前调用,不可被阻止
+    const defaultActionFn$: Function = DefaultBehavior[`${eventName}$`]   // [内置用] 在事件对象实例化所有插件回调之后调用,不可被阻止
     const ev = new (<any>GEvent)({
       name: eventName,
       container: container,
@@ -40,17 +41,33 @@ export class PluginManager {
     })
     const eventCallback: Function = options.callback
     if (eventCallback) delete options.eventCallback
+
+    // 合并外部传入配置到ev对象中，
     Object.assign(ev, options)
-    Object.keys(ev).forEach(name => ev[name] === void 0 ? ev[name] = null : void 0)  // 将undefined置成null
+
+    // 将ev中的undefined置成null
+    Object.keys(ev).forEach(name => ev[name] === void 0 ? ev[name] = null : void 0)
+
+    // 最后执行的 $defaultActionFn，不可被用户插件阻止
     if (isFunction($defaultActionFn)) $defaultActionFn.call(null, ev)
+
+    // 执行用户插件
     this.plugins.forEach((plugin) => {
       const callFunc: Function = <any>plugin[eventName]
       if (isFunction(callFunc)) callFunc.call(plugin, ev)
     })
-    if (!ev.isPrevent && isFunction(ev.default)) {  // 默认行为函数在最后执行
+
+    // 最后执行的 defaultActionFn$，不可被用户插件阻止
+    if (isFunction(defaultActionFn$)) defaultActionFn$.call(null, ev)
+
+    // 最后执行默认行为函数
+    if (!ev.isPrevent && isFunction(ev.default)) {
       (ev.default || defaultActionFn)?.call(null, ev)  // 内置的插件没有this
     }
-    if (isFunction(eventCallback)) eventCallback.call(null, ev)  // 如果有回调，将最终ev回调给外部传入的回调函数
+
+    // 如果外部有指定emit的callback函数回调，将最终ev回调的回调函数
+    if (isFunction(eventCallback)) eventCallback.call(null, ev)
+
     if (ev.isPrevent) {
       if (eventName === 'dragging') tempStore.preventDragging = true
       if (eventName === 'resizing') tempStore.preventResizing = true
